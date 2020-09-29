98bbc6b262a16e631dc4768ee492d228dea000e4
Yan-Rong Li (Dell Laptop) <liyanrong@mail.ihep.ac.cn>
2020-09-29 21:35:07 +0800

    update plot_results.py.

--- data/plot_results.py
+++ data/plot_results.py
@@ -11,9 +11,9 @@ from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 
 pdf = PdfPages("pyCALI_results.pdf")
 
-#
+#===================================================================
 # load params
-# 
+#===================================================================
 with open("param_input") as f:
   file_content = '[dump]\n' + f.read()
   
@@ -23,9 +23,9 @@ config.read_string(file_content)
 for key in config["dump"].keys():
   print(key, config["dump"][key])
 
-#
+#===================================================================
 # obtain norm for cont and line
-#
+#===================================================================
 cont = np.loadtxt(basename(config["dump"]["fcont"]))
 fp=open(basename(config["dump"]["fcont"]), "r")
 fstr=fp.readline()
@@ -34,9 +34,9 @@ num = int(fstr.split()[1])
 norm_cont = np.mean(cont[:num, 1])
 fp.close()
 
-#
+#===================================================================
 # load codes
-#
+#===================================================================
 code = np.genfromtxt("factor.txt", usecols=(0), skip_header=1, dtype=str)
 
 num_params_var = 2
@@ -73,7 +73,9 @@ sample[:, num_params_var+3*ncode:num_params_var+4*ncode] = np.log10( sample[:, n
 if config["dump"]["fline"] != "":
   sample[:, num_params_var+5*ncode:num_params_var+6*ncode] = np.log10( sample[:, num_params_var+5*ncode:num_params_var+6*ncode] )
 
+#===================================================================
 # print posterior values
+#===================================================================
 print("log10 Scale")
 for i in range(ncode):
   mean, low, up = np.quantile(sample[:, num_params_var+i], q=(0.5, 0.16, 0.84))
@@ -108,9 +110,9 @@ if config["dump"]["fline"] != "":
 plt.rc('text', usetex=True)
 plt.rc('font', family="serif", size=18)
 
-######################################################
+#===================================================================
 # now plot
-# 
+#===================================================================
 data={}
 nax = 1
 # first continuum 
@@ -218,7 +220,6 @@ ax.set_xlabel("Time")
 ax.set_title("Continuum Residuals")
 ax.set_ylabel("Residuals")
 ax.set_xlim(xlim[0], xlim[1])
-ax.set_ylim((-0.69, 0.69))
 ax.yaxis.set_major_locator(MultipleLocator(0.5))
 ax.yaxis.set_minor_locator(MultipleLocator(0.1))
 ylim = ax.get_ylim()
@@ -236,7 +237,7 @@ ax.set_ylim(ylim[0], ylim[1])
 [xt.set_visible(False) for xt in ax.get_xticklabels()]
 [xt.set_visible(False) for xt in ax.get_yticklabels()]
 ax.minorticks_on()
-ax.axhline(y=0.0, linestyle='--', color='silver', lw=1)
+ax.axhline(y=0.0, linestyle='--', color='silver', lw=1, zorder=0)
 ax.axhline(y=-error_mean, linestyle='--', color='silver', lw=1, zorder=0)
 ax.axhline(y=error_mean, linestyle='--', color='silver', lw=1, zorder=0)
 
@@ -252,6 +253,9 @@ ax.minorticks_on()
 pdf.savefig(fig)
 plt.close()
 
+#===================================================================
+# then plot line if there is
+#===================================================================
 if config["dump"]["fline"] != "":
  fig = plt.figure(figsize=(15, 12))
  
@@ -305,7 +309,6 @@ if config["dump"]["fline"] != "":
  ax.axhline(y=0.0, linestyle='--', color='silver', lw=1, zorder=0)
  ax.axhline(y=-error_mean, linestyle='--', color='silver', lw=1, zorder=0)
  ax.axhline(y=error_mean, linestyle='--', color='silver', lw=1, zorder=0)
- ax.set_ylim(-0.69, 0.69)
  ax.set_xlabel("Time")
  ax.set_title("Line Residuals")
  ax.set_ylabel("Residuals")
@@ -346,12 +349,18 @@ if config["dump"]["fline"] != "":
 #pdf.close()
 #exit()
 
+#===================================================================
+# now plot histograms
+#===================================================================
 if int(config["dump"]["fixed_scale"]) == 1:
   fig = corner.corner(sample[:, num_params_var+ncode+1:num_params_var+2*ncode], smooth=True, smooth1d = True, \
         levels=1.0-np.exp(-0.5*np.arange(1.0, 3.1, 1.0)**2), show_titles=True, title_fmt=".3f")
   
   ax = fig.get_axes()
-  #[ax[i*(ncode-1)+i].set_title(r'${0}$'.format(code[i])) for i in range(1, ncode)]
+  for i in range(ncode-1):
+    xlim = ax[i*(ncode-1)+i].get_xlim()
+    ylim = ax[i*(ncode-1)+i].get_ylim()
+    ax[i*(ncode-1)+i].text(xlim[1]-0.2*(xlim[1]-xlim[0]), ylim[1] - 0.2*(ylim[1]-ylim[0]), r'$\bf {0}$'.format(code[i+1]))
   fig.suptitle(r"\bf Shift", fontsize=20)
   pdf.savefig(fig)
   plt.close()
@@ -360,12 +369,21 @@ elif int(config["dump"]["fixed_shift"]) == 1:
   fig = corner.corner(sample[:, num_params_var+1:num_params_var+ncode], smooth=True, smooth1d = True,  \
         levels=1.0-np.exp(-0.5*np.arange(1.0, 3.1, 1.0)**2), show_titles=True, title_fmt=".3f")
   ax = fig.get_axes()
+  for i in range(ncode-1):
+    xlim = ax[i*(ncode-1)+i].get_xlim()
+    ylim = ax[i*(ncode-1)+i].get_ylim()
+    ax[i*(ncode-1)+i].text(xlim[1]-0.2*(xlim[1]-xlim[0]), ylim[1] - 0.2*(ylim[1]-ylim[0]), r'$\bf {0}$'.format(code[i+1]))
   fig.suptitle(r"\bf Scale", fontsize=20)
   pdf.savefig(fig)
   plt.close()
 else:
   for i in range(1, ncode):
+    range_min = np.min(sample[:, [num_params_var+i,num_params_var+i+ncode]], axis=0)
+    range_max = np.max(sample[:, [num_params_var+i,num_params_var+i+ncode]], axis=0)
+    span = range_max - range_min
+    range_interval = [[range_min[i]-0.3*span[i], range_max[i]+0.3*span[i]] for i in range(2)]
     fig = corner.corner(sample[:, [num_params_var+i,num_params_var+i+ncode]], smooth=True, smooth1d = True, labels=[r"$\log\varphi$", r"$G$"], 
+          range=range_interval, \
           levels=1.0-np.exp(-0.5*np.arange(1.0, 3.1, 1.0)**2), show_titles=True, title_fmt=".3f")
     
     ax = fig.get_axes()
