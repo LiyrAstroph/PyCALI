ebec2205abbbbff82a491156dfdd8ee4924d1825
Yan-Rong Li <lyanrong@mail.ihep.ac.cn>
2025-10-07 11:57:18 +0800

    a minor update to main.cpp.

--- src/pycali/pycali/main.cpp
+++ src/pycali/pycali/main.cpp
@@ -26,10 +26,14 @@ int main(int argc, char *argv[])
   cfg.print_cfg();
 
   Cali cali(cfg);
-  cali.mcmc();
+  //cali.mcmc();
   cali.get_best_params();
   cali.output();
   cali.recon();
+  
+  cout<<"================================"<<endl;
+  cout<<"One can execute 'python plot_for_cali.py param.txt' to plot the result,\n"
+        "which output a file 'PyCALI_results.pdf'. "<<endl;
 
   return EXIT_SUCCESS;
 }
